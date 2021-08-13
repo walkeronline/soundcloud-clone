6579669f86a996b8350a16bbb69a535ff1dfe764
walker <walkerwilliamsx@gmail.com>
2021-08-13 17:35:01 -0500

    Initial commit, file structure set up with basic API route

---
+++ backend/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const apiRouter = require('./api');
+
+const router = express.Router();
+
+router.use('/api', apiRouter);
+
+router.get('/hello/world', function (req, res) {
+	res.cookie('XSRF-TOKEN', req.csrfToken());
+	res.json({ message: 'Hello World!' });
+});
+
+module.exports = router;

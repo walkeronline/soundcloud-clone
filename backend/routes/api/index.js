12114268a5c2b3ae2c684bd345e02101953f1d95
walker <walkerwilliamsx@gmail.com>
2021-08-16 08:02:58 -0500

    Auth Middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
 router.post('/test', (req, res) => {
 	res.json({ requestBody: req.body });
 });
+
+router.get(
+	'/set-token-cookie',
+	asyncHandler(async (req, res) => {
+		const user = await User.findOne({
+			where: {
+				username: 'Demo-lition',
+			},
+		});
+		setTokenCookie(res, user);
+		return res.json({ user });
+	})
+);
 
 module.exports = router;

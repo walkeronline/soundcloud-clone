ff9ecd3f84d4b2b257432fd6461f8cb190a1a995
walker <walkerwilliamsx@gmail.com>
2021-08-16 08:24:55 -0500

    Removed hello world route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,8 +2,41 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 
+const asyncHandler = require('express-async-handler');
+const { User } = require('../../db/models');
+const {
+	setTokenCookie,
+	restoreUser,
+	requireAuth,
+} = require('../../utils/auth.js');
+
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+	res.json({ requestBody: req.body });
+});
+
+router.get(
+	'/set-token-cookie',
+	asyncHandler(async (req, res) => {
+		const user = await User.findOne({
+			where: {
+				username: 'Demo-lition',
+			},
+		});
+		setTokenCookie(res, user);
+		return res.json({ user });
+	})
+);
+
+router.get('/restore-user', restoreUser, (req, res) => {
+	return res.json(req.user);
+});
+
+router.get('/require-auth', requireAuth, (req, res) => {
+	return res.json(req.user);
+});
 
 module.exports = router;

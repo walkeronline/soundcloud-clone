a9fc06a12a2f35ea0b95d4aaf7489350ade9f19c
walker <walkerwilliamsx@gmail.com>
2021-08-16 08:11:18 -0500

    Login route added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,24 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+router.use('/session', sessionRouter);
 
-router.post('/test', (req, res) => {
-	res.json({ requestBody: req.body });
-});
-
-router.get(
-	'/set-token-cookie',
-	asyncHandler(async (req, res) => {
-		const user = await User.findOne({
-			where: {
-				username: 'Demo-lition',
-			},
-		});
-		setTokenCookie(res, user);
-		return res.json({ user });
-	})
-);
+router.use('/users', usersRouter);
 
 module.exports = router;
